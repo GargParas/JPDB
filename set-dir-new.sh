7111f2e1cd21d4057b2dff182b25a3bdcbea1542
Paras Garg <91370703+GargParas@users.noreply.github.com>
2024-02-03 00:35:29 +0530

    Update set-dir-new.sh

--- set-dir-new.sh
+++ set-dir-new.sh
@@ -2,7 +2,7 @@
 # $1 is port and create variable $2 will be 'jpdb'+$1
 # if $2 exist then error "JsonPowerDB folder exists; seems its already running" and exit
 # if $1 is in use then error "$1 port is in occupied; please use different port" and exit
-# How to run this script: ./set-dir-new
+# How to run this script: ./set-dir-new port_no
 
 PORT_CHECK=$(sudo lsof -i -P -n | grep :$1)
 n=${#PORT_CHECK}
@@ -22,25 +22,25 @@ echo "${folderName}"
 sudo apt-get update 
 sudo apt-get upgrade 
 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# sudo apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
 
-sudo mkdir -p /etc/apt/keyrings
+# sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update 
-sudo apt-get upgrade 
+# sudo apt-get update 
+# sudo apt-get upgrade 
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
+# sudo docker run hello-world
 
 cd $home
 mkdir $folderName
